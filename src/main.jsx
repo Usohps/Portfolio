3948a62b03a5724a5fb5e349efc4f24f031002e2
Usohps <usohps@gmail.com>
2024-02-26 12:42:11 +0100

    remove dependency

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "../src/App";
 import "./index.css";
-import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* HelmetProvider is used for SEO Optimization */}
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <App />
   </React.StrictMode>
 );
